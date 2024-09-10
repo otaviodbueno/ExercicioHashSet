8faac2214bad23107cb5a70130726b11c84a0d85
Otavio Bueno <otavio.bueno@autoware.com.br>
2024-09-10 16:14:26 -0300

    Add project files.

---
+++ CursoPOO/Entities/PessoaJuridica.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoPOO.Entities
+{
+    class PessoaJuridica : Pessoa
+    {
+        public int NumeroDeFuncionarios { get; set; }
+
+        public PessoaJuridica() { }
+
+        public PessoaJuridica(string nome, double rendaAnual, int numeroDeFuncionarios) : base(nome, rendaAnual)
+        {
+            NumeroDeFuncionarios = numeroDeFuncionarios;
+        }
+
+        public override double CalculaImposto()
+        {
+            double imposto;
+
+            if(NumeroDeFuncionarios > 10)
+            {
+                imposto = RendaAnual * 0.14;
+            }
+            else
+            {
+                imposto = RendaAnual * 0.16;
+            }
+
+            return imposto;
+        }
+
+    }
+}
---
+++ CursoPOO/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using CursoPOO.Entities;
+
+
+List<Pessoa> listaDePessoas = new List<Pessoa>();
+
+Console.Write("Digite o número de pagadores de imposto: ");
+int numero = int.Parse(Console.ReadLine());
+
+for  (int i = 1; i <= numero; i++)
+{
+    Console.WriteLine($"Dados do pagador de imposto {i}");
+    Console.Write("Pessoa física ou jurídica? (f/j): ");
+    string tipoPessoa = Console.ReadLine();
+
+    if (tipoPessoa == "f")
+    {
+        Console.Write("Nome: ");
+        string nome = Console.ReadLine();
+        Console.Write("Renda anual: ");
+        double rendaAnual = double.Parse(Console.ReadLine());
+        Console.Write("Gastos com saúde: ");
+        double gastoComSaude = double.Parse(Console.ReadLine());
+
+        PessoaFisica pessoaFisica = new PessoaFisica(nome, rendaAnual, gastoComSaude);
+
+        listaDePessoas.Add(pessoaFisica);
+    }
+    else
+    {
+        Console.Write("Nome da empresa: ");
+        string nome = Console.ReadLine();
+        Console.Write("Renda anual: ");
+        double rendaAnual = double.Parse(Console.ReadLine());
+        Console.Write("Número de funcionários: ");
+        int funcionarios = int.Parse(Console.ReadLine());
+
+        PessoaJuridica pessoaJuridica = new PessoaJuridica(nome, rendaAnual, funcionarios);
+
+        listaDePessoas.Add((pessoaJuridica));
+    }
+}
+
+foreach (Pessoa p in listaDePessoas)
+{
+    var retorno = p.CalculaImposto();
+    Console.WriteLine($"{p.Nome}: ${retorno}");
+}
