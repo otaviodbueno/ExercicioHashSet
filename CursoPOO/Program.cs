afe938007ed5702ba6bb256ed7df3f72dd6875a9
Otavio Bueno <otavio.bueno@autoware.com.br>
2024-09-11 14:14:59 -0300

    Exercicio exceção de saque em conta bancária.

--- CursoPOO/Program.cs
+++ CursoPOO/Program.cs
@@ -2,48 +2,28 @@
 using System.Globalization;
 using CursoPOO.Entities;
 
-
-List<Pessoa> listaDePessoas = new List<Pessoa>();
-
-Console.Write("Digite o número de pagadores de imposto: ");
-int numero = int.Parse(Console.ReadLine());
-
-for  (int i = 1; i <= numero; i++)
+try
 {
-    Console.WriteLine($"Dados do pagador de imposto {i}");
-    Console.Write("Pessoa física ou jurídica? (f/j): ");
-    string tipoPessoa = Console.ReadLine();
+    Console.WriteLine("Entre com os dados da conta: ");
+    Console.Write("Número: ");
+    int numeroDaConta = int.Parse(Console.ReadLine());
+    Console.Write("Nome: ");
+    string nome = Console.ReadLine();
+    Console.Write("Saldo inicial: ");
+    double saldoInicial = double.Parse(Console.ReadLine());
+    Console.Write("Limite de saque da conta: ");
+    int limiteSaque = int.Parse(Console.ReadLine());
 
-    if (tipoPessoa == "f")
-    {
-        Console.Write("Nome: ");
-        string nome = Console.ReadLine();
-        Console.Write("Renda anual: ");
-        double rendaAnual = double.Parse(Console.ReadLine());
-        Console.Write("Gastos com saúde: ");
-        double gastoComSaude = double.Parse(Console.ReadLine());
+    Console.Write("Valor desejado de saque: ");
+    double saque = double.Parse(Console.ReadLine());
 
-        PessoaFisica pessoaFisica = new PessoaFisica(nome, rendaAnual, gastoComSaude);
+    ContaBancaria conta = new ContaBancaria(numeroDaConta, nome, saldoInicial, limiteSaque, saque);
 
-        listaDePessoas.Add(pessoaFisica);
-    }
-    else
-    {
-        Console.Write("Nome da empresa: ");
-        string nome = Console.ReadLine();
-        Console.Write("Renda anual: ");
-        double rendaAnual = double.Parse(Console.ReadLine());
-        Console.Write("Número de funcionários: ");
-        int funcionarios = int.Parse(Console.ReadLine());
+    double saldoNovo = conta.RealizarSaque(saque);
 
-        PessoaJuridica pessoaJuridica = new PessoaJuridica(nome, rendaAnual, funcionarios);
-
-        listaDePessoas.Add((pessoaJuridica));
-    }
+    Console.WriteLine($"Novo saldo: {saldoNovo}");
 }
-
-foreach (Pessoa p in listaDePessoas)
+catch(DomainException ex)
 {
-    var retorno = p.CalculaImposto();
-    Console.WriteLine($"{p.Nome}: ${retorno}");
+    Console.WriteLine("Erro no saque: " + ex.Message);
 }
